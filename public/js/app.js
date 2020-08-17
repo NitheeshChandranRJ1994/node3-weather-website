73d010e76e7168991fcd0d58e886f3af4169cec5
Nitheesh Chandran <nitheesh.chandran@speridian.com>
2020-08-17 16:12:41 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ const messageTwo = document.querySelector('#message-2')
     })
 })
  */
-https://speridian.udemy.com/course/the-complete-nodejs-developer-course-2/learn/lecture/13729078#content
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -19,7 +19,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+    fetch('/weather?address='+ location).then((response) => {
     response.json().then((data) => {
         if(data.error){
             messageOne.textContent = data.error
